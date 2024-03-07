000275649da9cffc1533137411d50d3f7746152e
Main Kottaboung <115133179+kottaboung@users.noreply.github.com>
2024-03-07 18:50:06 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -25,9 +25,24 @@ window.addEventListener('mousemove', function() {
     navbar.classList.add('show');
 });
 
-// Add event listener for mouseout event to hide navbar when not hovered over
+
 window.addEventListener('mouseout', function() {
     var navbar = document.getElementById('navbar');
     navbar.classList.remove('show');
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
 
